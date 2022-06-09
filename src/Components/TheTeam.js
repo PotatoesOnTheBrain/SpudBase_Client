025663b0ed4a9112652fc08f7c761d9c3e5b2748
Andre Booth <DreLeRoy@MacBook-Pro.local>
2022-06-09 11:52:50 -0700

    worked on the team page/profile page (button completed css)

--- src/Components/TheTeam.js
+++ src/Components/TheTeam.js
@@ -1,5 +1,9 @@
 import "../TheTeam.css"
 import image from "../profile_picture.png"
+import image1 from "../Danny_photo.png"
+import image2 from "../Andre_pic.jpg"
+import image3 from "../Andrew_photo.jpg"
+
 
 function TheTeam() {
   return (
@@ -9,9 +13,9 @@ function TheTeam() {
       
       
         {/* <div className="ID_photo"> */}
-                <img className="photo1" src={image} alt="Danny's Photo"  />
-                <img className="photo2" src={image}  alt="Andre's Photo"  />
-                <img className="photo3" src={image}  alt="Andrew's Photo"  />
+                <img className="photo1" src={image1} alt="Danny's Photo"  />
+                <img className="photo2" src={image2}  alt="Andre's Photo"  />
+                <img className="photo3" src={image3}  alt="Andrew's Photo"  />
         {/* </div> */}
       <div ClassName="Danny">
         <h3>Danny Kwolek</h3>
@@ -20,12 +24,12 @@ function TheTeam() {
       
       <div ClassName="Andre">
         <h3>Andre Booth</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor fringilla metus, id pellentesque odio lacinia id. Vivamus ac blandit urna, eget fringilla ante. Aenean in fermentum odio. Praesent hendrerit eleifend bibendum. Aliquam tincidunt mauris sit amet dolor tincidunt ultrices. Etiam ligula turpis, elementum in porta a, facilisis ut quam. Suspendisse ac sapien odio.</p>
+            <p> My interest in the project was the front-end layout specifically with React and hooking up the different api key value pairs for the profile </p>
       </div>
 
       <div ClassName="Andrew">
         <h3>Andrew Neilson</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor fringilla metus, id pellentesque odio lacinia id. Vivamus ac blandit urna, eget fringilla ante. Aenean in fermentum odio. Praesent hendrerit eleifend bibendum. Aliquam tincidunt mauris sit amet dolor tincidunt ultrices. Etiam ligula turpis, elementum in porta a, facilisis ut quam. Suspendisse ac sapien odio.</p>
+        <p>I'm a full-stack developer from Houston, Texas. My interest in this project stems from a need to connect with other developers, and I helped work on the front end using React and CSS.</p>
       </div>
       
 
